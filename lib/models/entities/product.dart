8e70db2ec45dd0ee5148b542ac9711a0d5457ae0
Mahmoud Selim <mahmoudsaleam11@gmail.com>
2021-05-18 10:41:02 +0200

    mahmoud

--- lib/models/entities/product.dart
+++ lib/models/entities/product.dart
@@ -1,6 +1,5 @@
+import 'package:firebase_storage/firebase_storage.dart';
 
-
- import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
